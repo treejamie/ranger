9ce2c7644845dba2e42af161dc84bcaf51026ab6
Jamie Curle <1866+treejamie@users.noreply.github.com>
2025-05-15 12:41:11 +0100

    5 - Testing Interactions (#24)
    * and setup on the desktop
    
    * Three tests to test increment, one good, two ungood. One test to test decrement and that's mine
    
    * matching version of elixir and OTP in actions with the asdf versions in .tool-versions

--- test/ranger_web/live/counter_live_test.exs
+++ test/ranger_web/live/counter_live_test.exs
@@ -2,4 +2,52 @@ defmodule RangerWeb.CounterLiveTest do
   use RangerWeb.ConnCase
 
   import Phoenix.LiveViewTest
+
+  @tag :good
+  test "user can increase count", %{conn: conn} do
+    {:ok, view, _html} = live(conn, ~p"/counter")
+
+    view
+    |> element("#increment")
+    |> render_click()
+
+    assert has_element?(view, "#count", "1")
+  end
+
+  @tag :ungood
+  test "user can increase count (less effective)", %{conn: conn} do
+    {:ok, view, _html} = live(conn, ~p"/counter")
+
+    html =
+      view
+      |> element("#increment")
+      |> render_click()
+
+    # this just asserts that "1" is somewhere in the html.
+    # ... which is a bit dumb.
+    assert html =~ "1"
+
+  end
+
+  @tag :ungood
+  test "user can increase counter (bypasses HTML - not good)", %{conn: conn} do
+    {:ok, view, _} = live(conn, ~p"/counter")
+
+    render_click(view, "increase")
+
+    assert has_element?(view, "#count", "1")
+  end
+
+
+  @tag :mine
+  test "user can decreease count", %{conn: conn} do
+    {:ok, view, html} = live(conn, ~p"/counter")
+
+    view
+    |> element("#decrement")
+    |> render_click()
+
+    assert has_element?(view, "#count", "-1")
+  end
+
 end
