6e9c71676d29deff344bff40fe9e5f2714390ddc
Tree Jamie <me@treejamie.com>
2025-05-27 21:06:07 +0100

    7 - testing forms

--- test/ranger_web/live/todo_live_test.exs
+++ test/ranger_web/live/todo_live_test.exs
@@ -2,4 +2,17 @@ defmodule RangerWeb.TodoLiveTest do
   use RangerWeb.ConnCase
 
   import Phoenix.LiveViewTest
+
+  test "Can add todo", %{conn: conn} do
+    # ge the view
+    {:ok, view, _html} = live(conn, ~p"/todo")
+
+   view
+    |> form("#add-todo", %{todo: %{body: "Form fellowship"}})
+    |> render_submit()
+
+    assert has_element?(view, "[data-role=todo]", "Form fellowship")
+
+  end
+
 end
